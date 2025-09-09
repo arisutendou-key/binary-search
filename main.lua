ce06eed4555d200a414897bdf6442ced22135eb4
arisutendou-key <127687869+arisutendou-key@users.noreply.github.com>
2025-09-09 12:49:39 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,34 @@
-local module = function(t,s)
+local module = function(g,t,s,m_,x_)
+	g = g or 0
 	s = s or 2
-	t_ = t
-
-	for i = 1,s in t_ do
-		-- split table into s pieces
-		min, max = math.floor((i-1)/s), math.floor(i/s)
-		
+	
+	m_ = m_ or 1
+	x_ = x_ or #t_
+	space = x_-m_
+	
+	for i = 1,s do
+		m,x = m_+(((i-1)/s)*space), m_+((i/s)*space
+		if space > 2 then	
+			if g >= t[m] and g <= t[x] then
+				module(g,t,s,m,x)
+			end
+		elseif space == 2 then
+			if g == t[m] then
+				return m
+			elseif g == t[x] then
+				return g
+			else 
+				warn("Does not exist")
+				return
+			end
+		elseif space == 1 then
+			if g == t[x_] then
+				return x_
+			else
+				warn("Does not exist")
+				return
+			end
+		end		
 	end
 end
 
